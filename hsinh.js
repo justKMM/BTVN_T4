a5c4306cdc172a73c4eb5d46f088a1faaee0957f
justKMM <khaiminhmai2@gmail.com>
2021-08-20 16:43:44 +0700

    Commit

--- hsinh.js
+++ hsinh.js
@@ -1,8 +1,9 @@
-function Student(name, age, gpa)
+function Student(name, age, gpa, level)
 {
     this.name = name;
     this.age = age;
     this.gpa = gpa;
+    this.level = level;
 };
 
 const students = [{"Khoi", 17, 9.0}; {"Khoa", 18, 9.5}; {"Hoa", 18, 9.5}; {"Hue", 19, 9.7}; {"Hieu", 16, 9.3};];
